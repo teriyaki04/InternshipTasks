4a8ad177d8d8b5781ecbe3797d6e4996a361f333
Alisho Muborakshoev <muborakshoev.alisho@gmail.com>
2023-01-25 02:36:02 +0600

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        var input = Console.ReadLine().Split(' '); // помещаем в массив строк введенную строку с разделителем пробел, т.е. его не учитываем.
+
+        var result = input
+            .GroupBy(x => x.Length) // группируем массив по длине строки
+            .OrderBy(x => x.Key)    // ключ в данном случае это длина строки, сортируем(по возрастанию) по ней так как при группировании все будет вперемешку
+            .Select(x => new
+                {
+                    // собственно происходит выборка данных, где Length - длина строки которую мы отсортировали до этого
+                    // а Words - слова с такой длинной строки.
+                    Length = x.Key, 
+                    Words = x.Distinct().ToList()
+                }
+            ); 
+
+        //Вывод
+        foreach (var item in result)
+        {
+            Console.WriteLine($"Words of length: {item.Length}, Count:{item.Words.Count}");
+            Console.WriteLine($"{string.Join("\n",item.Words)}");
+        }
+
+
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+
+    //Time complexity - O(2^n), так как каждое действие вызывает еще 2 вызова функции, т.е. растет экспоненциально
+    //Space complexity - O(n), так как глубина дерева рекурсии равна n
+    private static int RecursiveFibonacci(int n)
+    {
+        if (n <= 2) return 1;
+        return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2);
+    }
+    
+    //Time complexity - O(n) итерируем n раз
+    //Space compelixy - O(1) константное и не зависит от кол-ва входных данных
+    private static int IterativeFibonacci(int n)
+    {
+        if (n <= 2) return 1;   
+        int firstNum = 1, secondNum = 1, next;
+        for (int i = 1; i < n; i++)
+        {
+            next = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = next;
+        }
+
+        return firstNum;
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IterativeFibonacci(14));
+        Console.WriteLine(RecursiveFibonacci(14));
+    }
+}
